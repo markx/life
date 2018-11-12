3a00489d66065f686c47f42feb44f56daec2a0e9
Ke Xu <kdotxu@gmail.com>
2018-11-12 14:58:38 -0500

    render moving rects

--- src/game_of_life/core.cljs
+++ src/game_of_life/core.cljs
@@ -1,6 +1,58 @@
 (ns game-of-life.core
   (:require
-   [game-of-life.game :as game]))
+   [game-of-life.life :as l]))
+
+
+(defn matrix->grid [matrix]
+    (mapcat (fn [r row]
+              (map #(vector r %1 %2) (range) row))
+            (range) matrix))
+
+(def matrix
+    [[0 0 0 0]
+     [0 1 0 0]
+     [0 1 0 0]
+     [0 0 0 0]])
+
+(defn render [canvas cells]
+  (js/console.log "cells: " cells)
+  (let [ctx (.getContext canvas "2d")
+        nrow 4
+        ncol 4
+        rectWidth (/ (.-width canvas) ncol)
+        rectHeight (/ (.-height canvas) nrow)]
+
+    (set! (.-fillStyle ctx) "green")
+    (doseq [[y x v] (filter #(< 0 (last %)) cells)]
+          (.fillRect ctx (* x rectWidth) (* y rectHeight)  rectWidth rectHeight))))
+
+(defn clear-canvas [canvas]
+  (.clearRect (.getContext canvas "2d")
+   0 0
+   (.-width canvas)
+   (.-height canvas)))
+
+(defn main-loop 
+  [canvas life]
+  (js/console.log "game loop")
+  (clear-canvas canvas)
+  (let [new-life (l/update-life life)]
+    (render canvas (:cells life))
+    (js/setTimeout #(main-loop canvas new-life) 1000)))
+    
+
+(defn main []
+  (def canvas (.getElementById js/document "canvas"))
+  (def init-life (l/new-life 4 4 matrix))
+  (main-loop canvas init-life))
+
+(set! (.-onload js/window) main)
+
+
+(comment
+  (.-offsetWidth canvas)
+  (def matrix [[0 0 true] [1 1 true][2 2 true][3 3 true]]))
+
 
 
 ;; start is called by init and after code reloading finishes
@@ -16,9 +68,6 @@
 
 ;; this is called before any code is reloaded
 (defn ^:dev/before-load stop []
-  (js/console.log "stop"))
-
-
-(set! (.-onload js/window) start)
-
+  (js/console.log "stop")
+  (and canvas (clear-canvas canvas)))
 
---
+++ src/game_of_life/life.cljc
@@ -0,0 +1,33 @@
+(ns game-of-life.life)
+
+(defn matrix->cells [matrix]
+    (mapcat (fn [r row]
+              (map #(vector r %1 %2) (range) row))
+            (range) matrix))
+
+(defn new-life 
+  ([width height]
+   {:width width
+    :height height
+    :cells [(for [ y (range width)
+                     x (range height)]
+              [x y 0])]})
+  ([width height matrix]
+   {:width width
+    :height height
+    :cells (matrix->cells matrix)}))
+
+
+
+(defn shift-right [cell, w]
+  (update cell 1 #(rem (+ 1 %) w)))
+
+
+(defn update-life [life]
+  (assoc life :cells (map shift-right (:cells life) (repeat (:width life)))))
+
+(comment
+  (take 5 (repeat 4)))
+
+
+
