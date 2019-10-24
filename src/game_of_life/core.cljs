2726d130ba9d58569b2161ac909aa90a93af4435
Ke Xu <kdotxu@gmail.com>
2019-10-23 20:40:25 -0700

    handle click

--- src/game_of_life/core.cljs
+++ src/game_of_life/core.cljs
@@ -5,38 +5,61 @@
    [impi.core :as impi]))
 
 
-(defonce matrix (atom [[0 0 0 0]
-                       [0 1 0 0]
-                       [0 1 0 0]
-                       [0 0 0 0]]))
-
+(def matrix [[0 0 0 0]
+             [0 1 0 0]
+             [0 1 0 0]
+             [0 0 0 0]])
 (def CELL-SIZE 20)
+
+(defonce *state (atom nil))
 
 
 (defn make-cell [[row col value]]
-  {:pixi.shape/type :pixi.shape.type/rounded-rectangle
-      :pixi.shape/position (map #(* CELL-SIZE %) [col row])
-      :pixi.shape/size [CELL-SIZE CELL-SIZE]
-      :pixi.shape/line
-      {:pixi.line/color 0xbbbbbb
-       :pixi.line/width 2}
-      :pixi.rounded-rectangle/radius 4
-      :pixi.shape/fill
-      {:pixi.fill/color (if (= 0 value)
-                          0xffffff
-                          0x000000)}})
+  (let [x (* CELL-SIZE col)
+        y (* CELL-SIZE row)]
+    {:impi/key [x y]
+     :pixi.object/type :pixi.object.type/graphics
+     :pixi.object/position [x y]
+     :pixi.object/interactive? true
+     :pixi.event/click    [:click [row col] (- 1 value)]
+     :pixi.event/mouse-down    [:mouse-down]
+     :pixi.event/mouse-up      [:mouse-up]
+     :pixi.object/hit-area [0 0 CELL-SIZE CELL-SIZE]
+     :pixi.graphics/shapes
+     [{:pixi.shape/type :pixi.shape.type/rounded-rectangle
+       :pixi.shape/size [CELL-SIZE CELL-SIZE]
+       :pixi.shape/line
+       {:pixi.line/color 0xbbbbbb
+        :pixi.line/width 2}
+       :pixi.rounded-rectangle/radius 4
+       :pixi.shape/fill
+       {:pixi.fill/color (if (= 0 value)
+                           0xffffff
+                           0x000000)}}]}))
+
+(defn set-cell [coords v]
+  (swap! *state update :cells (fn [cells]
+                                (map #(if (= coords (take 2 %))
+                                        (conj (pop %) v)
+                                        %)
+                                     cells))))
 
 
 (defn make-scene [cells]
   {:pixi/renderer
-    {:pixi.renderer/size [400 400]
-     :pixi.renderer/background-color 0xbbbbbb}
-    :pixi/stage
-    {:impi/key :gfx
-     :pixi.object/type :pixi.object.type/graphics
-     :pixi.object/position [0 0]
-     :pixi.graphics/shapes
-     (vec (map make-cell cells))}})
+   {:pixi.renderer/size [400 400]
+    :pixi.renderer/background-color 0xbbbbbb}
+   :pixi/listeners
+   {:click      (fn [_ coords v] (set-cell coords v))
+    :mouse-down (fn [x] (prn :mouse-down x))
+    :mouse-up   (fn [x] (prn :mouse-up x))
+    :mouse-over (fn [x] (prn :mouse-over x))
+    :mouse-out  (fn [x] (prn :mouse-out x))}
+   :pixi/stage
+   {:pixi.object/type :pixi.object.type/container
+    :pixi.container/children
+    (vec (map make-cell cells))}})
+
 
 
 (defn render [el cells]
@@ -44,23 +67,22 @@
   (impi/mount :scene (make-scene cells) el))
 
 (defn main-loop
-  [el life]
+  [el state]
   (js/console.log "game loop")
-  (let [new-life (l/update-life life)]
-    (render el (:cells life))
-    (js/setTimeout #(main-loop el new-life) 1000)))
+  (let [new-life (l/update-life @state)]
+    (reset! state new-life)
+    (render el (:cells new-life))
+    (js/setTimeout #(main-loop el state) 1000)))
 
 
 (defn main []
   (let [el (.getElementById js/document "app")
-        init-life (l/new-life 4 4 @matrix)]
-    (main-loop el init-life)))
+        init-life (l/new-life 4 4 matrix)]
+    (reset! *state init-life)
+    (main-loop el *state)))
 
 (set! (.-onload js/window) main)
 
-
-(comment
-  (reset! matrix [[0 0 true] [1 1 true][2 2 true][3 3 true]]))
 
 
 ;; start is called by init and after code reloading finishes
